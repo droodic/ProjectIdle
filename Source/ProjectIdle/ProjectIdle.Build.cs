047904c5c388b4313ac4312aaa1fa9ff5581112d
Robert <roobtran@gmail.com>
2020-05-06 23:05:47 -0400

    UMG/Slate dependency

--- Source/ProjectIdle/ProjectIdle.Build.cs
+++ Source/ProjectIdle/ProjectIdle.Build.cs
@@ -8,6 +8,9 @@ public class ProjectIdle : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "UMG" });
+
+      //PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
     }
 }
