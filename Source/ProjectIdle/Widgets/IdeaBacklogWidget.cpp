6747115215c0c928ccc89d72a87e64cb49262197
Droodic <droodic.gamedev@gmail.com>
2020-05-20 13:07:07 -0400

    Bind idea generation to backlog
    When an idea is generated an idea is added to the backlog

--- Source/ProjectIdle/Widgets/IdeaBacklogWidget.cpp
+++ Source/ProjectIdle/Widgets/IdeaBacklogWidget.cpp
@@ -2,4 +2,25 @@
 
 
 #include "IdeaBacklogWidget.h"
+#include "Engine.h"
+#include "Components\Button.h"
 
+void UIdeaBacklogWidget::DisplayNewIdea() {
+	GEngine->AddOnScreenDebugMessage(5, 5.f, FColor::Red, "Displaying new idea");
+	if (!IdeaButton1->IsVisible()) {
+		IdeaButton1->SetVisibility(ESlateVisibility::Visible);
+	}
+	else if (!IdeaButton2->IsVisible()) {
+		IdeaButton2->SetVisibility(ESlateVisibility::Visible);
+	}
+	else {
+		IdeaButton3->SetVisibility(ESlateVisibility::Visible);
+	}
+}
+
+void UIdeaBacklogWidget::NativeConstruct() {
+	Super::NativeConstruct();
+	//IdeaButton1->SetVisibility(ESlateVisibility::Hidden);
+	//IdeaButton2->SetVisibility(ESlateVisibility::Hidden);
+	//IdeaButton3->SetVisibility(ESlateVisibility::Hidden); //array?
+}
